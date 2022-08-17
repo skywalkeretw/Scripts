edeafa30e9b36cc7f323db1bf488b9913cfd66e3
Luke Roy <lukeroy@Lukes-MacBook-Pro.local>
2022-08-17 20:34:55 +0200

    Refactor code

--- music-fn/__main__.py
+++ music-fn/__main__.py
@@ -1,8 +1,68 @@
-import git
-import gitlab
+import git # https://gitpython.readthedocs.io/en/stable/
+import gitlab # https://python-gitlab.readthedocs.io/en/stable/index.html
 import os, sys, stat
 import uuid
 import shutil
+
+# Create SSH KEY and connect to git returns error object or false if succesful
+def setupSSH(key, hostname):
+    print("---> Setting Up Connection to git")
+    sshdir = '/root/.ssh'
+    if not os.path.exists(sshdir):
+        os.mkdir(sshdir)
+        print(f"---> Create {sshdir} dir")
+    else:
+        print("---> .ssh dir exists")
+
+    with open( sshdir + '/id_rsa', 'w') as f:
+        f.write(key)
+        f.close()
+    
+    os.chmod(sshdir + '/id_rsa', 0o600)
+    add2hosts = f'ssh-keyscan -H {hostname} >>  {sshdir}/known_hosts'
+    sshcode = os.system(add2hosts)
+    if sshcode != 0:
+        r["message"] = f"Failed to Connect to {hostname}"
+        print(f"---> {r['message']}")
+        return r
+    
+    print("---> Connection to gitlab confirmed")
+    return False
+
+# gitlab function to create Merge Request
+def gitlab(hostname, token, repoURL, branch, merge):
+    # Login to Gitlab
+    print(f"---> Login to {hostname}")
+    gl = gitlab.Gitlab(private_token=token)
+    print(f"---> Get Project ID")
+    projects = gl.projects.list(search=repoURL.split("/",1)[1].split(".",1)[0])
+    
+    # Connect to Gitlab Project
+    projectid = projects[len(projects)-1].id
+    print(f"---> Connect to Project: {projectid}")
+    project = gl.projects.get(projectid)
+    
+    # Create Merge Request
+    mTitle = f'Merging new Music into branch {branch}'
+    mDescription = f'Adding Music into master branch from {branch}'
+    print(f"---> Create Merge Request for Project: {projectid} on Branch: {branch} ")
+    mr = project.mergerequests.create({'source_branch': branch, 'target_branch': 'master', 'title': mTitle, 'description': mDescription})
+    print(f"---> Merge URL: {mr.web_url}")
+
+    # If params is set Merge Pull/Merge request
+    if merge != False or merge != "false":
+        mr.merge()
+        r["message"] = "Successfully downloaded Music, Created Pull/Merge request and merge it automatically"
+        r["error"] = False
+        print(f"---> {r['message']}")
+        return r
+
+    # Downloaded Music and Create Pull/Merge request
+    r["message"] = "Successfully downloaded Music and Created Pull/Merge request"
+    r["error"] = False
+    r["url"] = mr.web_url
+    print(f"---> {r['message']}")
+    return r
 
 # Required Params and their usage 
 # key, repourl, username and email can be set as default so they dont need to be sent with every request
@@ -51,34 +111,18 @@ def main(params):
         return r
 
     # Optional Parma to auto merge the merge request
+    # merge = params["merge"] if 'merge' in params else False
     if 'merge' in params:
         merge = params["merge"]
     else:
         merge = False
 
-    # setup SSH Connection to git 
-    print("---> Setting Up Connection to git")
-    sshdir = '/root/.ssh'
-    if not os.path.exists(sshdir):
-        os.mkdir(sshdir)
-        print(f"---> Create {sshdir} dir")
-    else:
-        print("---> .ssh dir exists")
-
-    with open( sshdir + '/id_rsa', 'w') as f:
-        f.write(key)
-        f.close()
-    
-    os.chmod(sshdir + '/id_rsa', 0o600)
     hostname = repoURL.split("@",1)[1].split(":",1)[0]
-    add2hosts = f'ssh-keyscan -H {hostname} >>  {sshdir}/known_hosts'
-    sshcode = os.system(add2hosts)
-    if sshcode == 0:
-        print("---> Connection to gitlab confirmed")
-    else:
-        r["message"] = f"Failed to Connect to {hostname}"
-        print(f"---> {r['message']}")
-        return r
+
+    # setup SSH Connection to git 
+    returnValue = setupSSH(key, hostname)
+    if returnValue != False:
+        return returnValue
 
     # Clone the Repository
     musicdir = '/playlist'
@@ -90,12 +134,10 @@ def main(params):
     repo = git.Repo.clone_from(repoURL, musicdir)
     os.chdir(musicdir)
 
-
     # Set Username and Email
     print("---> Set Git Username and Email")
     repo.config_writer().set_value("user", "name", username).release()
     repo.config_writer().set_value("user", "email", email).release()
-
 
     # Create and Checkout new Branch
     branch = uuid.uuid4().hex
@@ -140,6 +182,8 @@ def main(params):
         print(f"---> {r['message']}")
         return r
 
+    if len(music) == successfulDownloads:
+        print("---> All Songs have been downloaded!")
 
     # Add and Commit changes
     print("---> Add and Commit changes")
@@ -152,41 +196,13 @@ def main(params):
 
     # Create Merge Request
     if hostname == "gitlab.com":
-        # Login to Gitlab
-        print(f"---> Login to {hostname}")
-        gl = gitlab.Gitlab(private_token=token)
-        print(f"---> Get Project ID")
-        projects = gl.projects.list(search=repoURL.split("/",1)[1].split(".",1)[0])
-        
-        # Connect to Gitlab Project
-        projectid = projects[len(projects)-1].id
-        print(f"---> Connect to Project: {projectid}")
-        project = gl.projects.get(projectid)
-        
-        # Create Merge Request
-        mTitle = f'Merging new Music into branch {branch}'
-        mDescription = f'Adding Music into master branch from {branch}'
-        print(f"---> Create Merge Request for Project: {projectid} on Branch: {branch} ")
-        mr = project.mergerequests.create({'source_branch': branch, 'target_branch': 'master', 'title': mTitle, 'description': mDescription})
-        print(f"---> Merge URL: {mr.web_url}")
-
-        # If params is set Merge Pull/Merge request
-        if merge != False or merge != "false":
-            mr.merge()
-            r["message"] = "Successfully downloaded Music, Created Pull/Merge request and merge it automatically"
-            r["error"] = False
-            print(f"---> {r['message']}")
-            return r
-
-        # Downloaded Music and Create Pull/Merge request
-        r["message"] = "Successfully downloaded Music and Created Pull/Merge request"
-        r["error"] = False
-        r["url"] = mr.web_url
-        print(f"---> {r['message']}")
-        return r
+        return gitlab(hostname, token, repoURL, branch, merge)
 
     # Downloaded music without creating a Pull/Merge request
     r["message"] = "Successfully downloaded Music"
     r["error"] = False
     print(f"---> {r['message']}")
     return r
+
+
+    
